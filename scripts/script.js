09bcf9e70d361e43b06f111504b736b4fee8bcb8
reuelmagistrado <mag21010@byui.edu>
2022-12-14 13:39:03 +0800

    feature: navigation, hero section, footer

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,29 @@ menuBtn = document.querySelector(".hamburger-btn");
 menuBtn.addEventListener("click", displayNavLinks);
 
 function displayNavLinks() {
-  var links = document.querySelector("#navLinks");
-  if (links.style.display === "block") {
-    links.style.display = "none";
-  } else {
-    links.style.display = "block";
-  }
+  const links = document.querySelector(".navLinks");
+
+  links.classList.toggle("showHide");
 }
+
+const now = new Date();
+const yearHolder = document.querySelector(".date-modified");
+const currentYear = now.getFullYear();
+const time1 = document.createElement("time");
+
+function displayYear() {
+  time1.dateTime = now;
+  time1.innerHTML = `&copy; ${currentYear} |`;
+  yearHolder.insertAdjacentElement("afterbegin", time1);
+}
+
+const time2 = document.createElement("time");
+
+function displayDateModified() {
+  time2.dateTime = now;
+  time2.innerHTML = `Last&nbsp;Modified:&nbsp;${document.lastModified}`;
+  yearHolder.insertAdjacentElement("beforeend", time2);
+}
+
+displayYear();
+displayDateModified();
