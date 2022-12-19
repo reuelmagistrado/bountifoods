ce302bbb27daf1acbc4e05ba2fdc9f286608e357
reuelmagistrado <mag21010@byui.edu>
2022-12-20 01:36:39 +0800

    feature: added map for development purposes

---
+++ scripts/map.js
@@ -0,0 +1,16 @@
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 33.1580933, lng: -117.3505939 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 15,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
